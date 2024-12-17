cb7c7d2e06fd96f65a3e4736768a5155d5b58dca
Mücahit Buğday <mucahit.bugday@outlook.com>
2024-12-18 00:16:19 +0300

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import YandexMap from "@/components/Maps/YandexMap";
-
 
 export default function Home() {
 
   return (
     <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
       <YandexMap />
-
-
-
     </div>
   );
 }
