bb11bf0c60bb305bcf263214ea47c46e2bf72fed
Mücahit Buğday <mucahit.bugday@outlook.com>
2024-12-18 00:20:02 +0300

    Temporarily comment out YandexMap component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import YandexMap from "@/components/Maps/YandexMap";
+// import YandexMap from "@/components/Maps/YandexMap";
 
 export default function Home() {
 
   return (
     <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
-      <YandexMap />
+      {/* <YandexMap /> */}
     </div>
   );
 }
